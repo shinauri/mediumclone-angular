17aa27361a28927f3682cdfbdc392d228365d79d
giorgi tvalmikhelashvili <giorgitvalmikhelashvili@gmail.com>
2020-12-01 06:12:32 +0400

    implement sign up form component ts and html.

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
     selector: 'mc-register',
     templateUrl: 'register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+    form: FormGroup
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.initializeForm()
+    }
+
+    initializeForm(): void {
+        this.form = this.fb.group({
+            username: ['', Validators.required],
+            email: '',
+            password: '',
+        })
+    }
+
+    onSubmit(): void {
+        console.log(this.form.value)
+    }
+}
