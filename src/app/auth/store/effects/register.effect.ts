9a7e2f3b28586f371e1527b9e67f5e6bf9d09f3e
giorgi tvalmikhelashvili <giorgitvalmikhelashvili@gmail.com>
2020-12-08 18:24:47 +0400

    refactoring some piece of code, add errors interface and implement registration proccess

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,5 +1,6 @@
-import { Actions, createEffect, ofType } from '@ngrx/effects'
 import { Injectable } from '@angular/core'
+import { HttpErrorResponse } from '@angular/common/http'
+import { Actions, createEffect, ofType } from '@ngrx/effects'
 import { catchError, map, switchMap } from 'rxjs/operators'
 import { of } from 'rxjs'
 
@@ -11,20 +12,27 @@ import {
 import { AuthService } from 'src/app/auth/services/auth.service'
 import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
 
+const successCallback = (currentUser: CurrentUserInterface) => {
+    return registerSuccessAction({ currentUser })
+}
+
+const errorCallback = (errorResponse: HttpErrorResponse) => {
+    return of(
+        registerFailureAction({
+            errors: errorResponse.error.errors,
+        })
+    )
+}
+
 @Injectable()
 export class RegisterEffect {
     register$ = createEffect(() =>
         this.actions$.pipe(
             ofType(registerAction),
             switchMap(({ request }) => {
-                return this.authService.register(request).pipe(
-                    map((currentUser: CurrentUserInterface) => {
-                        return registerSuccessAction({ currentUser })
-                    }),
-                    catchError(() => {
-                        return of(registerFailureAction())
-                    })
-                )
+                return this.authService
+                    .register(request)
+                    .pipe(map(successCallback), catchError(errorCallback))
             })
         )
     )
--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,16 +1,29 @@
 import { AuthStateInterface } from 'src/app/auth/types/authState.interface'
 import { Action, createReducer, on } from '@ngrx/store'
 
-import { registerAction } from 'src/app/auth/store/actions/register.action'
-import { registerOperation } from 'src/app/auth/store/operations/auth.operations'
+import {
+    registerAction,
+    registerFailureAction,
+    registerSuccessAction,
+} from 'src/app/auth/store/actions/register.action'
+import {
+    registerOperation,
+    registerSuccessOperation,
+    registerFailureOperation,
+} from 'src/app/auth/store/operations/auth.operations'
 
 const initialState: AuthStateInterface = {
     isSubmitting: false,
+    currentUser: null,
+    isLoggedIn: null,
+    validationErrors: null,
 }
 
 const authReducer = createReducer(
     initialState,
-    on(registerAction, registerOperation)
+    on(registerAction, registerOperation),
+    on(registerSuccessAction, registerSuccessOperation),
+    on(registerFailureAction, registerFailureOperation)
 )
 
 export function reducers(
