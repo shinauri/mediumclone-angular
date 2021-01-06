76378690b442aca7aeb190e58dba85bd7b5ac47c
giorgi tvalmikhelashvili <giorgitvalmikhelashvili@gmail.com>
2021-01-06 14:29:02 +0400

    refactoring auth module effects.

--- src/app/auth/store/effects/login.effect.ts
+++ src/app/auth/store/effects/login.effect.ts
@@ -1,57 +1,23 @@
 import { Injectable } from '@angular/core'
-import { HttpErrorResponse } from '@angular/common/http'
-import { createEffect, ofType } from '@ngrx/effects'
-import { catchError, map, switchMap, tap } from 'rxjs/operators'
-import { of } from 'rxjs'
 
 import {
     loginAction,
     loginFailureAction,
     loginSuccessAction,
 } from 'src/app/auth/store/actions/login.action'
-import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface'
 import { AuthEffect } from 'src/app/auth/store/effects/auth.effect'
 import { environment } from 'src/environments/environment'
 
 @Injectable()
 export class LoginEffect extends AuthEffect {
-    readonly authUrl = environment.apiUrl + environment.endpoints.auth.login
+    protected authUrl = environment.apiUrl + environment.endpoints.auth.login
+    protected authAction
+    protected authSuccessAction
+    protected authFailureAction
 
-    private redirectAfterSubmit = createEffect(
-        () =>
-            this.actions$.pipe(
-                ofType(loginSuccessAction),
-                tap(() => {
-                    this.router.navigateByUrl('/')
-                })
-            ),
-        { dispatch: false }
-    )
-
-    private login$ = createEffect(() =>
-        this.actions$.pipe(
-            ofType(loginAction),
-            switchMap(({ request }) => {
-                return this.authService
-                    .auth(request, this.authUrl)
-                    .pipe(
-                        map(this.successCallback),
-                        catchError(this.errorCallback)
-                    )
-            })
-        )
-    )
-
-    private successCallback = (currentUser: CurrentUserInterface) => {
-        this.persistenceService.set(environment.token, currentUser.token)
-        return loginSuccessAction({ currentUser })
-    }
-
-    private errorCallback = (errorResponse: HttpErrorResponse) => {
-        return of(
-            loginFailureAction({
-                errors: errorResponse.error.errors,
-            })
-        )
+    protected initializeActions(): void {
+        this.authAction = loginAction
+        this.authSuccessAction = loginSuccessAction
+        this.authFailureAction = loginFailureAction
     }
 }
